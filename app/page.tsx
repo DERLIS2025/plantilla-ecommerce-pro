342b728dc054baa8ecba180611b505cc1b1f1f58
eQuantum <derlisa567@gmail.com>
2026-04-13 18:00:17 -0300

    Refactor HomePage layout and add WhatsAppFloating

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CategorySidebar } from '@/components/category-sidebar';
+import { WhatsAppFloating } from '@/components/whatsapp-floating';
 import { cesped, paisajismo } from '@/lib/data';
 import { HomeHero } from '@/sections/home-hero';
 import { ProductSection } from '@/sections/product-section';
@@ -7,19 +8,23 @@ import { ServicesSection } from '@/sections/services-section';
 
 export default function HomePage() {
   return (
-    <div className="container-shell section-space space-y-6">
-      <div className="grid gap-4 lg:grid-cols-[280px_1fr]">
-        <CategorySidebar />
-        <HomeHero />
+    <>
+      <div className="container-shell section-space space-y-6">
+        <div className="grid gap-4 lg:grid-cols-[280px_1fr]">
+          <CategorySidebar />
+          <HomeHero />
+        </div>
+
+        <PromoStrip />
+
+        <ProductSection title="Césped" products={cesped} />
+
+        <ServicesSection />
+
+        <ProductSection title="Paisajismo" products={paisajismo} />
       </div>
 
-      <PromoStrip />
-
-      <ProductSection title="Césped" products={cesped} />
-
-      <ServicesSection />
-
-      <ProductSection title="Paisajismo" products={paisajismo} />
-    </div>
+      <WhatsAppFloating />
+    </>
   );
 }
